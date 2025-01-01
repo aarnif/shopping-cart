fe8719d563c0266380399719d69e924844656a27
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-01-01 22:11:37 +0200

    Update style in BlurredImage.jsx

--- src/components/BlurredImage.jsx
+++ src/components/BlurredImage.jsx
@@ -4,14 +4,16 @@ const BlurredImage = ({ lowQualitySrc, highQualitySrc }) => {
   const [src, { blur }] = useProgressiveImg(lowQualitySrc, highQualitySrc);
 
   return (
-    <img
-      src={src}
-      className="w-full h-full object-cover"
-      style={{
-        filter: blur ? "blur(20px)" : "none",
-        transition: blur ? "none" : "filter 1s ease-out",
-      }}
-    />
+    <div className="relative w-full h-full">
+      <img
+        src={src}
+        className="absolute top-0 left-0 w-full h-full object-cover"
+        style={{
+          filter: blur ? "blur(20px)" : "none",
+          transition: blur ? "none" : "filter 1s ease-out",
+        }}
+      />
+    </div>
   );
 };
 
