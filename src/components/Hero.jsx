731e6653db5a48c0658335185c15f0c50bcb485e
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-01-01 21:51:34 +0200

    Load image progressively in Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,25 +1,15 @@
-import { motion } from "framer-motion";
+import BlurredImage from "./BlurredImage";
 
 const Hero = ({ randomArtPiece }) => {
-  if (!randomArtPiece) {
-    return null;
-  }
-
   return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 1 }}
-      key={randomArtPiece.title}
-      className="w-full flex-grow h-[700px] mt-[60px] flex justify-center items-center bg-slate-500"
-      style={{
-        backgroundImage: randomArtPiece
-          ? `url(${randomArtPiece.image})`
-          : "none",
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-      }}
-    ></motion.div>
+    <div className="w-full flex-grow h-[700px] mt-[60px] flex justify-center items-center bg-slate-500">
+      {randomArtPiece && (
+        <BlurredImage
+          lowQualitySrc={randomArtPiece.tinyImage}
+          highQualitySrc={randomArtPiece.image}
+        />
+      )}
+    </div>
   );
 };
 
