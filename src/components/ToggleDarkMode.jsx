a68154e968dd0afc0052cd9ddfff53ed39dbad00
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-02-14 22:19:28 +0200

    Add ToggleDarkMode.jsx

---
+++ src/components/ToggleDarkMode.jsx
@@ -0,0 +1,44 @@
+import { useState, useEffect } from "react";
+
+import { IoSunnyOutline } from "react-icons/io5";
+import { HiMoon } from "react-icons/hi2";
+
+const ToggleDarkMode = () => {
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
+
+  useEffect(() => {
+    if (localStorage.getItem("theme") === "dark") {
+      document.documentElement.classList.add("dark");
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  const changeTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    console.log("Change theme to:", newTheme);
+    setTheme(newTheme);
+    document.documentElement.classList.toggle("dark");
+  };
+
+  return (
+    <button
+      key="toggle-dark-mode"
+      onClick={changeTheme}
+      className="flex justify-center items-center cursor-pointer transition-all duration-300 ease-in-out"
+    >
+      {theme === "dark" ? (
+        <HiMoon key="dark" className="w-7 h-7 fill-current animate-scale-in" />
+      ) : (
+        <IoSunnyOutline
+          key="light"
+          className="w-7 h-7 fill-current animate-scale-in"
+        />
+      )}
+    </button>
+  );
+};
+
+export default ToggleDarkMode;
