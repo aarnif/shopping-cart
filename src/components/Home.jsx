464283c3d2fc929fd2913a48d8a8bb75ccc9dfe4
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-02-15 12:07:26 +0200

    Extract art work data into data.js

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,60 +1,11 @@
 import { IoIosStar } from "react-icons/io";
 import { FaArrowRightLong } from "react-icons/fa6";
 
-const artworks = [
-  {
-    title: "Art Work",
-    artist: "Artist Name",
-    image: "/images/emotion.webp",
-    description: "Card Description",
-    price: "Price €",
-    reviews: 35,
-    averageRating: 4,
-    type: "portrait",
-  },
-  {
-    title: "Art Work",
-    artist: "Artist Name",
-    image: "/images/spirit.webp",
-    description: "Card Description",
-    price: "Price €",
-    reviews: 42,
-    averageRating: 5,
-    type: "landscape",
-  },
-  {
-    title: "Art Work",
-    artist: "Artist Name",
-    image: "/images/doctor.webp",
-    description: "Card Description",
-    price: "Price €",
-    reviews: 57,
-    averageRating: 4,
-    type: "portrait",
-  },
-  {
-    title: "Art Work",
-    artist: "Artist Name",
-    image: "/images/poppies.webp",
-    description: "Card Description",
-    price: "Price €",
-    reviews: 49,
-    averageRating: 5,
-    type: "landscape",
-  },
-];
+import artworks from "../data.js";
 
 const ArtCard = ({ data }) => {
-  const {
-    title,
-    artist,
-    image,
-    description,
-    price,
-    reviews,
-    averageRating,
-    type,
-  } = data;
+  const { title, artist, image, description, price, reviews, averageRating } =
+    data;
 
   return (
     <div
@@ -65,8 +16,6 @@ const ArtCard = ({ data }) => {
         <img
           src={image}
           alt={`${title} by ${artist}`}
-          width={500}
-          height={500}
           className="h-full w-full object-cover rounded-t-lg"
         />
       </div>
---
+++ src/data.js
@@ -0,0 +1,44 @@
+const artworks = [
+  {
+    title: "Art Work",
+    artist: "Artist Name",
+    image: "/images/emotion.webp",
+    description: "Card Description",
+    price: "Price €",
+    reviews: 35,
+    averageRating: 4,
+    type: "portrait",
+  },
+  {
+    title: "Art Work",
+    artist: "Artist Name",
+    image: "/images/spirit.webp",
+    description: "Card Description",
+    price: "Price €",
+    reviews: 42,
+    averageRating: 5,
+    type: "landscape",
+  },
+  {
+    title: "Art Work",
+    artist: "Artist Name",
+    image: "/images/doctor.webp",
+    description: "Card Description",
+    price: "Price €",
+    reviews: 57,
+    averageRating: 4,
+    type: "portrait",
+  },
+  {
+    title: "Art Work",
+    artist: "Artist Name",
+    image: "/images/poppies.webp",
+    description: "Card Description",
+    price: "Price €",
+    reviews: 49,
+    averageRating: 5,
+    type: "landscape",
+  },
+];
+
+export default artworks;
