b1ed6ced5bb9d388b6f8c326c00122b99c6b17c2
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-02-21 15:34:23 +0200

    Add tablet view to header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,30 +1,77 @@
+import { useNavigate } from "react-router";
+
 import { IoMdMenu } from "react-icons/io";
+import { FaShoppingCart } from "react-icons/fa";
 
 import ToggleDarkMode from "./ToggleDarkMode";
 
+const Title = () => {
+  return (
+    <h1 className="w-full text-rose-700 text-center sm:text-left font-roboto-condensed text-2xl font-bold">
+      Artful Finds
+    </h1>
+  );
+};
+
+const MobileHeaderNav = ({ handleShowMenu }) => {
+  return (
+    <nav className="sm:hidden w-full">
+      <ul className="w-full flex justify-between items-center">
+        <li className="flex justify-center items-center">
+          <button
+            className="flex justify-center items-center cursor-pointer transition-all duration-300 ease-in-out"
+            onClick={handleShowMenu}
+          >
+            <IoMdMenu className="w-7 h-7 fill-current" />
+          </button>
+        </li>
+        <li className="flex justify-center items-center">
+          <Title />
+        </li>
+        <li className="flex justify-center items-center">
+          <ToggleDarkMode />
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+const HeaderNav = () => {
+  const navigate = useNavigate();
+  return (
+    <nav className="hidden sm:flex w-full">
+      <Title />
+      <ul className="w-full max-w-[450px] flex justify-between items-center">
+        <li className="flex justify-center items-center hover:underline cursor-pointer active:underline">
+          Home
+        </li>
+        <li className="flex justify-center items-center hover:underline cursor-pointer active:underline">
+          Who We Are
+        </li>
+        <li className="flex justify-center items-center hover:underline cursor-pointer active:underline">
+          Shop
+        </li>
+        <li className="flex justify-center items-center">
+          <button
+            className="flex justify-center items-center cursor-pointer transition-all duration-300 ease-in-out"
+            onClick={() => navigate("/cart")}
+          >
+            <FaShoppingCart className="w-7 h-7 fill-current" />
+          </button>
+        </li>
+        <li className="flex justify-center items-center cursor-pointer">
+          <ToggleDarkMode />
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
 const Header = ({ handleShowMenu }) => {
   return (
-    <header className="z-10 p-4 fixed w-full flex justify-center items-center bg-white shadow-xl">
-      <nav className="w-full">
-        <ul className="w-full flex justify-between items-center">
-          <li className="flex justify-center items-center hover:underline cursor-pointer">
-            <button
-              className="flex justify-center items-center cursor-pointer transition-all duration-300 ease-in-out"
-              onClick={handleShowMenu}
-            >
-              <IoMdMenu className="w-7 h-7 fill-current" />
-            </button>
-          </li>
-          <li className="flex justify-center items-center hover:underline cursor-pointer">
-            <h1 className="text-rose-700 text-center font-roboto-condensed text-2xl font-bold">
-              Artful Finds
-            </h1>
-          </li>
-          <li className="flex justify-center items-center cursor-pointer">
-            <ToggleDarkMode />
-          </li>
-        </ul>
-      </nav>
+    <header className="z-10 px-4 sm:px-8 py-4 fixed w-full flex justify-center items-center bg-white shadow-xl">
+      <MobileHeaderNav handleShowMenu={handleShowMenu} />
+      <HeaderNav />
     </header>
   );
 };
