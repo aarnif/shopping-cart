1fc5eec2aaf6d5d642c105832f785a8f09d91ead
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-02-22 15:09:23 +0200

    Add desktop view to home page

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -73,6 +73,73 @@ const ArtCard = ({ data, navigate }) => {
   );
 };
 
+const ArtFeed = ({ data, navigate }) => {
+  return (
+    <div className="my-12 sm:my-16">
+      <h2 className="mb-2 sm:mb-4 text-white xl:text-slate-900 text-center font-roboto-condensed text-xl font-semibold">
+        This Month&apos;s Featured Artworks
+      </h2>
+      <div className="w-full flex flex-col gap-8">
+        <div className="w-full flex flex-col sm:flex-row justify-center items-center sm:items-end gap-8">
+          <ArtCard key={data[0].id} data={data[0]} navigate={navigate} />
+          <ArtCard key={data[1].id} data={data[1]} navigate={navigate} />
+        </div>
+        <div className="w-full flex flex-col sm:flex-row justify-center items-center sm:items-start gap-8">
+          <ArtCard key={data[2].id} data={data[2]} navigate={navigate} />
+          <ArtCard key={data[3].id} data={data[3]} navigate={navigate} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const HeroContent = ({ navigate }) => {
+  return (
+    <div className="animate-hop-in flex justify-center items-center flex-col">
+      <h1 className="text-white text-center font-roboto-condensed text-2xl font-bold">
+        We are <span className="italic font-medium">cultivators</span>,{" "}
+        <span className="italic font-medium">curators</span>, and{" "}
+        <span className="italic font-medium">connoisseurs</span> of
+      </h1>
+      <h1 className="text-white text-center font-roboto-condensed text-2xl font-bold">
+        artistic expression.
+      </h1>
+      <button
+        className="flex gap-2 mt-4 px-4 py-2 justify-center items-center text-white font-bold bg-rose-700 border-2 border-rose-700
+       rounded-lg cursor-pointer active:border-rose-900 active:inset-shadow-sm transition-all duration-300 ease-in-out"
+        onClick={() => navigate("/art")}
+      >
+        <p>Buy Here</p>
+        <FaArrowRightLong className="w-5 h-5 text-white fill-current" />
+      </button>
+    </div>
+  );
+};
+
+const MobileAndTabletContent = ({ data, navigate }) => {
+  return (
+    <div className="block xl:hidden relative bg-slate-100 before:content-[''] before:absolute before:top-0 before:left-0 before:w-full before:h-[100vh] before:bg-hero before:bg-cover before:bg-center before:bg-no-repeat">
+      <div className="mt-20 sm:mt-32 relative w-full px-8 sm:px-12 py-8 flex flex-col">
+        <HeroContent navigate={navigate} />
+        <ArtFeed data={data} navigate={navigate} />
+      </div>
+    </div>
+  );
+};
+
+const DesktopContent = ({ data, navigate }) => {
+  return (
+    <div className="w-full hidden xl:flex flex-col items-center bg-slate-100">
+      <div className="w-full h-screen flex justify-center items-center bg-hero bg-cover bg-center bg-no-repeat">
+        <HeroContent navigate={navigate} />
+      </div>
+      <div className="w-full px-12 py-8 max-w-[1200px]">
+        <ArtFeed data={data} navigate={navigate} />
+      </div>
+    </div>
+  );
+};
+
 const Home = () => {
   const navigate = useNavigate();
   const { data, loading } = useQuery(FEATURED_ARTWORKS);
@@ -82,59 +149,13 @@ const Home = () => {
   }
 
   return (
-    <div className="relative bg-slate-100 before:content-[''] before:absolute before:top-0 before:left-0 before:w-full before:h-[100vh] before:bg-hero before:bg-cover before:bg-center before:bg-no-repeat">
-      <div className="mt-20 sm:mt-32 relative w-full px-8 sm:px-12 py-8 flex flex-col">
-        <div className="animate-hop-in flex justify-center items-center flex-col">
-          <h1 className="text-white text-center font-roboto-condensed text-2xl font-bold">
-            We are <span className="italic font-medium">cultivators</span>,{" "}
-            <span className="italic font-medium">curators</span>, and{" "}
-            <span className="italic font-medium">connoisseurs</span> of
-          </h1>
-          <h1 className="text-white text-center font-roboto-condensed text-2xl font-bold">
-            artistic expression.
-          </h1>
-          <button
-            className="flex gap-2 mt-4 px-4 py-2 justify-center items-center text-white font-bold bg-rose-700 border-2 border-rose-700
-             rounded-lg cursor-pointer active:border-rose-900 active:inset-shadow-sm transition-all duration-300 ease-in-out"
-            onClick={() => navigate("/art")}
-          >
-            <p>Buy Here</p>
-            <FaArrowRightLong className="w-5 h-5 text-white fill-current" />
-          </button>
-        </div>
-        <div className="my-12 sm:my-16">
-          <h2 className="mb-2 sm:mb-4 text-white text-center font-roboto-condensed text-xl font-semibold">
-            This Month&apos;s Featured Artworks
-          </h2>
-          <div className="w-full flex flex-col gap-8">
-            <div className="w-full flex flex-col sm:flex-row justify-center items-center sm:items-end gap-8">
-              <ArtCard
-                key={data.featuredArtWorks[0].id}
-                data={data.featuredArtWorks[0]}
-                navigate={navigate}
-              />
-              <ArtCard
-                key={data.featuredArtWorks[1].id}
-                data={data.featuredArtWorks[1]}
-                navigate={navigate}
-              />
-            </div>
-            <div className="w-full flex flex-col sm:flex-row justify-center items-center sm:items-start gap-8">
-              <ArtCard
-                key={data.featuredArtWorks[2].id}
-                data={data.featuredArtWorks[2]}
-                navigate={navigate}
-              />
-              <ArtCard
-                key={data.featuredArtWorks[3].id}
-                data={data.featuredArtWorks[3]}
-                navigate={navigate}
-              />
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <>
+      <MobileAndTabletContent
+        data={data.featuredArtWorks}
+        navigate={navigate}
+      />
+      <DesktopContent data={data.featuredArtWorks} navigate={navigate} />
+    </>
   );
 };
 
