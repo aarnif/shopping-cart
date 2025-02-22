7d777938054b322fdfd974cf6661e135cd00d827
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-02-22 22:16:14 +0200

    Update header styles

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,21 +1,19 @@
-import { useNavigate } from "react-router";
+import { useNavigate, useLocation } from "react-router";
 
 import { IoMdMenu } from "react-icons/io";
 import { FaShoppingCart } from "react-icons/fa";
 
 import ToggleDarkMode from "./ToggleDarkMode";
 
-const Title = () => {
-  return (
-    <h1 className="w-full text-rose-700 text-center sm:text-left font-roboto-condensed text-2xl font-bold">
-      Artful Finds
-    </h1>
-  );
-};
+const Title = () => (
+  <h1 className="w-full text-rose-700 text-center md:text-left font-roboto-condensed text-2xl font-bold">
+    Artful Finds
+  </h1>
+);
 
 const MobileHeaderNav = ({ handleShowMenu }) => {
   return (
-    <nav className="sm:hidden w-full">
+    <nav className="md:hidden w-full bg-white">
       <ul className="w-full flex justify-between items-center">
         <li className="flex justify-center items-center">
           <button
@@ -38,6 +36,7 @@ const MobileHeaderNav = ({ handleShowMenu }) => {
 
 const HeaderNav = () => {
   const navigate = useNavigate();
+  const location = useLocation();
 
   const navItems = [
     { name: "home", text: "Home", callback: () => navigate("/") },
@@ -56,9 +55,12 @@ const HeaderNav = () => {
   ];
 
   return (
-    <nav className="hidden sm:flex w-full">
+    <nav className="hidden md:flex w-full">
       <Title />
-      <ul className="w-full max-w-[450px] flex justify-between items-center">
+      <ul
+        id={location.pathname == "/" ? "nav-items" : ""}
+        className="w-full max-w-[500px] flex justify-between items-center"
+      >
         {navItems.map((item) => {
           if (item.name === "cart") {
             return (
@@ -84,7 +86,7 @@ const HeaderNav = () => {
             return (
               <li
                 key={item.name}
-                className="flex justify-center items-center hover:underline cursor-pointer active:underline"
+                className="flex justify-center items-center font-medium hover:underline cursor-pointer active:underline"
               >
                 <button className="cursor-pointer" onClick={item.callback}>
                   {item.text}
@@ -100,7 +102,7 @@ const HeaderNav = () => {
 
 const Header = ({ handleShowMenu }) => {
   return (
-    <header className="z-10 px-4 sm:px-8 py-4 fixed w-full flex justify-center items-center bg-white shadow-xl">
+    <header className="z-10 px-4 md:px-8 py-4 fixed w-full flex justify-center items-center bg-white md:bg-white/0 md:backdrop-blur-xs">
       <MobileHeaderNav handleShowMenu={handleShowMenu} />
       <HeaderNav />
     </header>
