6d7cd8c6a8e15cb38ed5aa255cf997c98d8b1719
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-02-22 11:31:39 +0200

    Add tablet view for LatestItemModal.jsx

--- frontend/src/components/LatestItemModal.jsx
+++ frontend/src/components/LatestItemModal.jsx
@@ -1,9 +1,13 @@
 import { useNavigate } from "react-router";
 import { motion } from "framer-motion";
 import { FaCheck } from "react-icons/fa6";
+import useResponsiveWidth from "../hooks/useResponsiveWidth";
 
 const LatestItemModal = ({ latestItem, setShowLatestItemModal }) => {
   const navigate = useNavigate();
+  const width = useResponsiveWidth();
+  const isMobile = width <= 768;
+
   const proceedToCheckout = () => {
     console.log("Proceed to checkout");
     navigate("/cart");
@@ -17,23 +21,44 @@ const LatestItemModal = ({ latestItem, setShowLatestItemModal }) => {
 
   const { title, artist, image, size } = latestItem;
 
+  const modalVariantsMobile = {
+    initial: { y: "100vh" },
+    animate: { y: 0 },
+    exit: { y: "100vh" },
+  };
+
+  const modalVariantsTabletAndDesktop = {
+    initial: { x: "100vw" },
+    animate: { x: 0 },
+    exit: { x: "100vw" },
+  };
+
   return (
     <motion.div
-      key={"overlay"}
-      className="z-10 fixed inset-0 bg-black/50 flex justify-center items-end"
+      key="overlay"
+      className="z-10 fixed inset-0 bg-black/50 flex justify-center md:justify-end items-end"
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
     >
       <motion.div
-        key={"latestItemModal"}
-        initial={{ y: "100vh" }}
-        animate={{ y: 0 }}
-        exit={{ y: "100vh" }}
+        key="latestItemModal"
+        variants={
+          isMobile ? modalVariantsMobile : modalVariantsTabletAndDesktop
+        }
+        initial="initial"
+        animate="animate"
+        exit="exit"
         transition={{ type: "tween", ease: "easeOut", duration: 0.3 }}
-        className="w-full h-[90vh] bg-white flex flex-col rounded-tl-lg rounded-tr-lg"
+        className={`w-full md:max-w-[500px] h-[90vh] md:h-screen bg-white flex flex-col ${
+          isMobile && "rounded-tl-lg rounded-tr-lg"
+        }`}
       >
-        <div className="w-full p-4 bg-green-400 flex justify-center items-center gap-2 rounded-tl-lg rounded-tr-lg">
+        <div
+          className={`w-full p-4 bg-green-400 flex justify-center items-center gap-2 ${
+            isMobile && "rounded-tl-lg rounded-tr-lg"
+          }`}
+        >
           <div className="w-6 h-6 flex justify-center items-center rounded-full bg-white">
             <FaCheck className="w-4 h-4 text-green-400 fill-current" />
           </div>
@@ -43,7 +68,11 @@ const LatestItemModal = ({ latestItem, setShowLatestItemModal }) => {
         </div>
         <div className="w-full flex-grow py-8 px-6 flex flex-col gap-4">
           <div className="w-full flex justify-center items-center">
-            <img className="w-auto max-h-[200px] object-contain" src={image} />
+            <img
+              className="w-auto max-h-[200px] object-contain"
+              src={image}
+              alt={title}
+            />
           </div>
           <div className="w-full flex flex-col gap-4">
             <div>
