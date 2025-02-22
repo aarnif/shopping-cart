b09fc0b92d6a1f9aac0451a2493ea96d34ba4ad4
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-02-22 12:13:37 +0200

    Add tablet view and update social media icons in footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,31 +1,34 @@
 import { FaFacebook, FaInstagram, FaLinkedin } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 
-const Footer = () => {
+const SocialMediaButton = ({ icon }) => {
   return (
-    <footer className="p-2 w-full flex flex-col justify-center items-center bg-white text-slate-900">
-      <div className="w-full flex flex-col justify-center items-center gap-4">
-        <ul className="flex gap-8">
-          <li>
-            <button className="flex justify-center items-center cursor-pointer transition-all duration-300 ease-in-out">
-              <FaFacebook className="w-5 h-5 fill-current" />
-            </button>
-          </li>
-          <li>
-            <button className="flex justify-center items-center cursor-pointer transition-all duration-300 ease-in-out">
-              <FaInstagram className="w-5 h-5 fill-current" />
-            </button>
-          </li>
-          <li>
-            <button className="flex justify-center items-center cursor-pointer transition-all duration-300 ease-in-out">
-              <FaLinkedin className="w-5 h-5 fill-current" />
-            </button>
-          </li>
-          <li>
-            <button className="flex justify-center items-center cursor-pointer transition-all duration-300 ease-in-out">
-              <FaXTwitter className="w-5 h-5 fill-current" />
-            </button>
-          </li>
+    <li>
+      <button
+        className="p-2 flex justify-center items-center rounded-full cursor-pointer border border-white 
+      hover:bg-slate-100 active:bg-slate-100 active:border-slate-200 active:inset-shadow-sm transition-all duration-300 ease-in-out"
+      >
+        {icon}
+      </button>
+    </li>
+  );
+};
+
+const Footer = () => {
+  const socialMediaIcons = [
+    <FaFacebook className="w-5 h-5 fill-current" />,
+    <FaInstagram className="w-5 h-5 fill-current" />,
+    <FaLinkedin className="w-5 h-5 fill-current" />,
+    <FaXTwitter className="w-5 h-5 fill-current" />,
+  ];
+
+  return (
+    <footer className="p-2 sm:p-4 w-full flex justify-center items-center bg-white text-slate-900">
+      <div className="w-full flex flex-col sm:flex-row justify-center sm:justify-between items-center gap-4">
+        <ul className="flex gap-4">
+          {socialMediaIcons.map((icon, index) => (
+            <SocialMediaButton key={index} icon={icon} />
+          ))}
         </ul>
         <p className="text-sm font-medium">© 2025 Artful Finds Inc.</p>
       </div>
