62b28d0b1dd62f4317c7d9ceb66c9a9d4b970d2a
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-02-23 17:47:16 +0200

    Update alert modal styles

--- frontend/src/components/AlertModal.jsx
+++ frontend/src/components/AlertModal.jsx
@@ -18,29 +18,27 @@ const AlertModal = ({ title, message, handleClose }) => {
         transition={{ type: "tween", ease: "easeOut", duration: 0.3 }}
         className="bg-yellow-50 rounded-lg border-t border-yellow-900"
       >
-        <div className="w-full p-4 flex flex-col gap-4">
+        <div className="w-full p-4 md:p-6 flex flex-col gap-4">
           <div className="flex justify-start items-center gap-3">
-            <div className="w-8.5 h-8.5 flex justify-center items-center rounded-full bg-yellow-300">
-              <div className="w-6 h-6 flex justify-center items-center rounded-full bg-yellow-100 border-2 border-yellow-400">
-                <FaExclamation className="w-4 h-4 text-yellow-900 fill-current" />
+            <div className="w-8.5 h-8.5 md:w-10 md:h-10 flex justify-center items-center rounded-full bg-yellow-300">
+              <div className="w-6 h-6 md:w-7 md:h-7 flex justify-center items-center rounded-full bg-yellow-100 border-2 border-yellow-400">
+                <FaExclamation className="w-4 h-4 md:w-4.5 md:h-4.5 text-yellow-900 fill-current" />
               </div>
             </div>
-            <h2 className="text-center text-yellow-900 text-lg font-semibold font-robo-condensed">
+            <h2 className="text-center text-yellow-900 text-lg md:text-xl font-semibold font-robo-condensed">
               {title}
             </h2>
           </div>
-          <div className="self-stretch h-12 text-yellow-700 text-sm">
+          <div className="self-stretch h-12 text-yellow-700 text-sm md:text-base">
             {message}
           </div>
 
           <button
-            className="w-full px-4 py-2 bg-yellow-200 rounded-lg border border-yellow-200 shadow-xl flex justify-center items-center 
+            className="w-full px-4 py-2 bg-yellow-200 rounded-lg border border-yellow-200 shadow-xl flex justify-center items-center text-yellow-900 cursor-pointer text-sm md:text-base xl:text-lg font-bold
               hover:bg-yellow-300 active:bg-yellow-300 active:border-yellow-400 active:inset-shadow-sm transition-all duration-300 ease-in-out"
             onClick={handleClose}
           >
-            <p className="text-center text-yellow-900 text-sm font-bold">
-              Close
-            </p>
+            Close
           </button>
         </div>
       </motion.div>
