6111bc98b44674ce3616dad481a170065adc21b4
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-02-23 17:44:05 +0200

    Add dark mode to latest item modal

--- frontend/src/components/LatestItemModal.jsx
+++ frontend/src/components/LatestItemModal.jsx
@@ -50,17 +50,17 @@ const LatestItemModal = ({ latestItem, setShowLatestItemModal }) => {
         animate="animate"
         exit="exit"
         transition={{ type: "tween", ease: "easeOut", duration: 0.3 }}
-        className={`w-full md:max-w-[400px] xl:max-w-[600px] h-[90vh] md:h-screen bg-white flex flex-col ${
+        className={`w-full md:max-w-[400px] xl:max-w-[600px] h-[90vh] md:h-screen bg-white dark:bg-slate-800 flex flex-col ${
           isMobile && "rounded-tl-lg rounded-tr-lg"
         }`}
       >
         <div
-          className={`w-full p-4 bg-green-400 flex justify-center items-center gap-2 ${
+          className={`w-full p-4 bg-green-400 dark:bg-green-600 flex justify-center items-center gap-2 ${
             isMobile && "rounded-tl-lg rounded-tr-lg"
           }`}
         >
           <div className="w-6 h-6 flex justify-center items-center rounded-full bg-white">
-            <FaCheck className="w-4 h-4 text-green-400 fill-current" />
+            <FaCheck className="w-4 h-4 text-green-400 dark:text-green-600 fill-current" />
           </div>
           <h1 className="text-white text-lg xl:text-xl font-semibold font-roboto-condensed">
             Added following item to cart:
@@ -76,18 +76,18 @@ const LatestItemModal = ({ latestItem, setShowLatestItemModal }) => {
           </div>
           <div className="w-full flex flex-col gap-4">
             <div>
-              <h2 className="text-slate-900 text-sm md:text-base xl:text-lg font-bold font-roboto-condensed">
+              <h2 className="text-slate-900 dark:text-slate-100 text-sm md:text-base xl:text-lg font-bold font-roboto-condensed">
                 {title}
               </h2>
-              <h3 className="text-slate-700 text-xs md:text-sm xl:text-base italic">
+              <h3 className="text-slate-700 dark:text-slate-300 text-xs md:text-sm xl:text-base italic">
                 by {artist}
               </h3>
             </div>
             <div className="flex flex-col gap-4">
-              <p className="text-slate-700 text-xs md:text-sm xl:text-base font-medium">
+              <p className="text-slate-700 dark:text-slate-300 text-xs md:text-sm xl:text-base font-medium">
                 {size.dimensions}
               </p>
-              <p className="text-slate-700 text-sm md:text-base xl:text-lg font-bold">
+              <p className="text-slate-700 dark:text-slate-300 text-sm md:text-base xl:text-lg font-bold">
                 {size.price} €
               </p>
             </div>
@@ -95,15 +95,13 @@ const LatestItemModal = ({ latestItem, setShowLatestItemModal }) => {
         </div>
         <div className="w-full p-4 flex flex-col gap-2">
           <button
-            className="w-full py-2 px-4 bg-slate-800 rounded-lg shadow-lg flex justify-center items-center text-white text-sm md:text-base xl:text-lg font-bold
-              hover:bg-slate-900 active:bg-slate-900 active:inset-shadow-sm transition-all duration-300 ease-in-out"
+            className="w-full py-2 px-4 bg-slate-800 dark:bg-slate-200 rounded-lg shadow-lg cursor-pointer flex justify-center items-center gap-2 text-white dark:text-slate-800 text-sm md:text-base xl:text-lg font-bold hover:bg-slate-900 dark:hover:bg-slate-300 active:bg-slate-900 dark:active:bg-slate-300 active:inset-shadow-sm transition-all duration-300 ease-in-out"
             onClick={proceedToCheckout}
           >
             Checkout
           </button>
           <button
-            className="w-full py-2 px-4 flex justify-center items-center rounded-lg border-[0.5px] border-slate-900 text-slate-700 text-sm md:text-base xl:text-lg font-medium
-              hover:bg-slate-200 active:bg-slate-200 active:inset-shadow-sm transition-all duration-300 ease-in-out"
+            className="w-full py-2 px-4 flex justify-center items-center rounded-lg cursor-pointer border-[0.5px] border-slate-900 dark:border-slate-200 text-slate-700 dark:text-slate-200 text-sm md:text-base xl:text-lg font-medium hover:bg-slate-200 dark:hover:bg-slate-700 active:bg-slate-200 dark:active:bg-slate-700 active:inset-shadow-sm transition-all duration-300 ease-in-out"
             onClick={continueShopping}
           >
             Continue Shopping
