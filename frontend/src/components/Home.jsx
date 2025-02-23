07317186c346b6e5c0d059a7dca3a2410e816cb2
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-02-23 15:13:29 +0200

    Update home page styles

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -24,7 +24,7 @@ const ArtCard = ({ data, navigate }) => {
     <div
       id="art-card"
       className={`w-full ${
-        type === "portrait" && "sm:max-w-[400px]"
+        type === "portrait" && "md:max-w-[400px]"
       } flex flex-col bg-white shadow-lg rounded-lg`}
     >
       <div className="w-full h-[300px] bg-slate-300 rounded-t-lg">
@@ -35,32 +35,32 @@ const ArtCard = ({ data, navigate }) => {
         />
       </div>
       <div className="w-full flex flex-col gap-2">
-        <div className="p-2 sm:p-4">
-          <h3 className="text-slate-900 text-lg sm:text-xl font-bold font-roboto-condensed">
+        <div className="p-2 md:p-4">
+          <h3 className="text-slate-900 text-lg md:text-xl font-bold font-roboto-condensed">
             {title}
           </h3>
-          <h4 className="text-slate-800 text-sm sm:text-base italic">
+          <h4 className="text-slate-800 text-sm md:text-base italic">
             By {artist}
           </h4>
         </div>
-        <p className="p-2 sm:p-4 text-slate-700 text-sm sm:text-base">
+        <p className="px-2 md:px-4 text-slate-700 text-sm md:text-base">
           {description}
         </p>
-        <div className="w-full p-2 sm:p-4 bg-slate-50 flex justify-between items-center rounded-b-lg">
+        <div className="w-full p-2 md:p-4 bg-slate-50 flex justify-between items-center rounded-b-lg">
           <div className="flex flex-col gap-2">
-            <h4 className="text-slate-900 text-base sm:text-lg font-bold">
+            <h4 className="text-slate-900 text-base md:text-lg font-bold">
               {sizes[0].price} €
             </h4>
             <div className="flex gap-2 items-center">
               <StarRating rating={averageRating} />
-              <p className="text-slate-700 text-xs sm:text-base font-medium">
+              <p className="text-slate-700 text-xs md:text-base font-medium">
                 {reviews.length} Reviews
               </p>
             </div>
           </div>
           <div>
             <button
-              className="flex p-2 sm:px-8 s:py-4 justify-center items-center text-sm sm:text-base text-slate-900 font-bold bg-slate-200 border-2 border-slate-200 
+              className="flex p-2 md:px-8 s:py-4 justify-center items-center text-sm md:text-base xl:text-lg text-slate-900 font-bold bg-slate-200 border-2 border-slate-200 
             rounded-lg cursor-pointer active:border-slate-400 active:inset-shadow-sm transition-all duration-300 ease-in-out"
               onClick={() => navigate(`/art/${id}`)}
             >
@@ -75,16 +75,16 @@ const ArtCard = ({ data, navigate }) => {
 
 const ArtFeed = ({ data, navigate }) => {
   return (
-    <div className="my-12 sm:my-16">
-      <h2 className="mb-2 sm:mb-4 text-white xl:text-slate-900 text-center font-roboto-condensed text-xl font-semibold">
+    <div className="my-12 md:my-16">
+      <h2 className="mb-2 md:mb-4 text-white xl:text-slate-900 text-center font-roboto-condensed text-lg md:text-xl xl:text-2xl font-semibold">
         This Month&apos;s Featured Artworks
       </h2>
       <div className="w-full flex flex-col gap-8">
-        <div className="w-full flex flex-col sm:flex-row justify-center items-center sm:items-end gap-8">
+        <div className="w-full flex flex-col md:flex-row justify-center items-center md:items-end gap-8">
           <ArtCard key={data[0].id} data={data[0]} navigate={navigate} />
           <ArtCard key={data[1].id} data={data[1]} navigate={navigate} />
         </div>
-        <div className="w-full flex flex-col sm:flex-row justify-center items-center sm:items-start gap-8">
+        <div className="w-full flex flex-col md:flex-row justify-center items-center md:items-start gap-8">
           <ArtCard key={data[2].id} data={data[2]} navigate={navigate} />
           <ArtCard key={data[3].id} data={data[3]} navigate={navigate} />
         </div>
@@ -96,16 +96,16 @@ const ArtFeed = ({ data, navigate }) => {
 const HeroContent = ({ navigate }) => {
   return (
     <div className="animate-hop-in flex justify-center items-center flex-col">
-      <h1 className="text-white text-center font-roboto-condensed text-2xl font-bold">
+      <h1 className="text-white text-center font-roboto-condensed text-xl md:text-2xl xl:text-3xl font-bold">
         We are <span className="italic font-medium">cultivators</span>,{" "}
         <span className="italic font-medium">curators</span>, and{" "}
         <span className="italic font-medium">connoisseurs</span> of
       </h1>
-      <h1 className="text-white text-center font-roboto-condensed text-2xl font-bold">
+      <h1 className="text-white text-center font-roboto-condensed text-xl md:text-2xl xl:text-3xl font-bold">
         artistic expression.
       </h1>
       <button
-        className="flex gap-2 mt-4 px-4 py-2 justify-center items-center text-white font-bold bg-rose-700 border-2 border-rose-700
+        className="flex gap-2 mt-4 px-4 py-2 justify-center items-center text-base md:text-lg xl:text-xl text-white font-bold bg-rose-700 border-2 border-rose-700
        rounded-lg cursor-pointer active:border-rose-900 active:inset-shadow-sm transition-all duration-300 ease-in-out"
         onClick={() => navigate("/art")}
       >
@@ -119,7 +119,7 @@ const HeroContent = ({ navigate }) => {
 const MobileAndTabletContent = ({ data, navigate }) => {
   return (
     <div className="block xl:hidden relative bg-slate-100 before:content-[''] before:absolute before:top-0 before:left-0 before:w-full before:h-[100vh] before:bg-hero before:bg-cover before:bg-center before:bg-no-repeat">
-      <div className="mt-20 sm:mt-32 relative w-full px-8 sm:px-12 py-8 flex flex-col">
+      <div className="mt-20 md:mt-32 relative w-full px-8 md:px-12 py-8 flex flex-col">
         <HeroContent navigate={navigate} />
         <ArtFeed data={data} navigate={navigate} />
       </div>
--- frontend/src/components/StarRating.jsx
+++ frontend/src/components/StarRating.jsx
@@ -7,12 +7,12 @@ const StarRating = ({ rating }) => {
         index < rating ? (
           <IoIosStar
             key={index}
-            className="w-3 h-3 text-yellow-400 fill-current"
+            className="w-3 h-3 md:w-3.5 md:h-3.5 xl:w-4 xl:h-4 text-yellow-400 fill-current"
           />
         ) : (
           <IoIosStar
             key={index}
-            className="w-3 h-3 text-slate-700 fill-current"
+            className="w-3 h-3 md:w-3.5 md:h-3.5 xl:w-4 xl:h-4 text-slate-700 fill-current"
           />
         )
       )}
