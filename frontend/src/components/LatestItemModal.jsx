6167b385cdca60ccd5d77d23bbdd989d19833700
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-02-23 14:47:07 +0200

    Update last item modal styles

--- frontend/src/components/LatestItemModal.jsx
+++ frontend/src/components/LatestItemModal.jsx
@@ -50,7 +50,7 @@ const LatestItemModal = ({ latestItem, setShowLatestItemModal }) => {
         animate="animate"
         exit="exit"
         transition={{ type: "tween", ease: "easeOut", duration: 0.3 }}
-        className={`w-full md:max-w-[500px] h-[90vh] md:h-screen bg-white flex flex-col ${
+        className={`w-full md:max-w-[400px] xl:max-w-[600px] h-[90vh] md:h-screen bg-white flex flex-col ${
           isMobile && "rounded-tl-lg rounded-tr-lg"
         }`}
       >
@@ -62,49 +62,51 @@ const LatestItemModal = ({ latestItem, setShowLatestItemModal }) => {
           <div className="w-6 h-6 flex justify-center items-center rounded-full bg-white">
             <FaCheck className="w-4 h-4 text-green-400 fill-current" />
           </div>
-          <h1 className="text-white text-lg font-semibold font-roboto-condensed">
+          <h1 className="text-white text-lg xl:text-xl font-semibold font-roboto-condensed">
             Added following item to cart:
           </h1>
         </div>
         <div className="w-full flex-grow py-8 px-6 flex flex-col gap-4">
           <div className="w-full flex justify-center items-center">
             <img
-              className="w-auto max-h-[200px] object-contain"
+              className="w-auto max-h-[240px] md:max-h-[320px] xl:max-h-[400px] object-contain"
               src={image}
               alt={title}
             />
           </div>
           <div className="w-full flex flex-col gap-4">
             <div>
-              <h2 className="text-slate-900 text-sm font-bold font-roboto-condensed">
+              <h2 className="text-slate-900 text-sm md:text-base xl:text-lg font-bold font-roboto-condensed">
                 {title}
               </h2>
-              <h3 className="text-slate-700 text-xs italic">by {artist}</h3>
+              <h3 className="text-slate-700 text-xs md:text-sm xl:text-base italic">
+                by {artist}
+              </h3>
             </div>
             <div className="flex flex-col gap-4">
-              <p className="text-slate-700 text-xs font-medium">
+              <p className="text-slate-700 text-xs md:text-sm xl:text-base font-medium">
                 {size.dimensions}
               </p>
-              <p className="text-slate-700 text-sm font-bold">{size.price} €</p>
+              <p className="text-slate-700 text-sm md:text-base xl:text-lg font-bold">
+                {size.price} €
+              </p>
             </div>
           </div>
         </div>
         <div className="w-full p-4 flex flex-col gap-2">
           <button
-            className="w-full py-2 px-4 bg-slate-800 rounded-lg shadow-lg flex justify-center items-center
+            className="w-full py-2 px-4 bg-slate-800 rounded-lg shadow-lg flex justify-center items-center text-white text-sm md:text-base xl:text-lg font-bold
               hover:bg-slate-900 active:bg-slate-900 active:inset-shadow-sm transition-all duration-300 ease-in-out"
             onClick={proceedToCheckout}
           >
-            <p className="text-white text-sm font-bold">Checkout</p>
+            Checkout
           </button>
           <button
-            className="w-full py-2 px-4 flex justify-center items-center rounded-lg border-[0.5px] border-slate-900
+            className="w-full py-2 px-4 flex justify-center items-center rounded-lg border-[0.5px] border-slate-900 text-slate-700 text-sm md:text-base xl:text-lg font-medium
               hover:bg-slate-200 active:bg-slate-200 active:inset-shadow-sm transition-all duration-300 ease-in-out"
             onClick={continueShopping}
           >
-            <p className="text-slate-700 text-sm font-medium">
-              Continue Shopping
-            </p>
+            Continue Shopping
           </button>
         </div>
       </motion.div>
