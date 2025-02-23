db4ffeabf7dfb1e73f38b4cd096c1befdba88fb4
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-02-23 21:21:04 +0200

    Add dark mode styles to mobile menu

--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -11,11 +11,11 @@ const MenuItem = ({ icon, name, callback }) => {
   return (
     <li className="flex items-end">
       <button
-        className="flex justify-center items-center gap-4 group"
+        className="flex justify-center items-center gap-4 group cursor-pointer"
         onClick={callback}
       >
         {icon}
-        <p className="text-xl font-roboto font-bold group-active:underline transition-all duration-300 ease-in-out">
+        <p className="text-xl text-slate-900 dark:text-slate-200 font-bold group-hover:underline group-active:underline transition-all duration-300 ease-in-out">
           {name}
         </p>
       </button>
@@ -28,12 +28,16 @@ const Menu = ({ handleShowMenu }) => {
   const menuItems = [
     {
       name: "Back",
-      icon: <FaArrowLeft className="w-6 h-6 fill-current" />,
+      icon: (
+        <FaArrowLeft className="w-6 h-6 text-slate-900 dark:text-slate-200 fill-current" />
+      ),
       callback: handleShowMenu,
     },
     {
       name: "Home",
-      icon: <FaHouse className="w-6 h-6 fill-current" />,
+      icon: (
+        <FaHouse className="w-6 h-6 text-slate-900 dark:text-slate-200 fill-current" />
+      ),
       callback: () => {
         navigate("/");
         handleShowMenu();
@@ -41,11 +45,15 @@ const Menu = ({ handleShowMenu }) => {
     },
     {
       name: "Who We Are",
-      icon: <FaInfoCircle className="w-6 h-6 fill-current" />,
+      icon: (
+        <FaInfoCircle className="w-6 h-6 text-slate-900 dark:text-slate-200 fill-current" />
+      ),
     },
     {
       name: "Art",
-      icon: <FaPalette className="w-6 h-6 fill-current" />,
+      icon: (
+        <FaPalette className="w-6 h-6 text-slate-900 dark:text-slate-200 fill-current" />
+      ),
       callback: () => {
         navigate("/art");
         handleShowMenu();
@@ -53,7 +61,9 @@ const Menu = ({ handleShowMenu }) => {
     },
     {
       name: "Cart",
-      icon: <FaShoppingCart className="w-6 h-6 fill-current" />,
+      icon: (
+        <FaShoppingCart className="w-6 h-6 text-slate-900 dark:text-slate-200 fill-current" />
+      ),
       callback: () => {
         navigate("/cart");
         handleShowMenu();
@@ -63,13 +73,15 @@ const Menu = ({ handleShowMenu }) => {
 
   return (
     <motion.div
-      className="z-10 fixed inset-0 p-8 flex flex-col gap-6 bg-slate-100"
+      className="z-10 fixed inset-0 p-8 flex flex-col gap-6 bg-slate-100 dark:bg-slate-900"
       initial={{ x: "-100vw" }}
       animate={{ x: 0 }}
       exit={{ x: "-100vw" }}
       transition={{ type: "tween", duration: 0.3, ease: "easeOut" }}
     >
-      <h1 className="text-2xl font-roboto-condensed font-bold">Menu</h1>
+      <h1 className="text-2xl text-slate-900 dark:text-slate-200 font-roboto-condensed font-bold">
+        Menu
+      </h1>
       <ul className="p-4 flex flex-col gap-6">
         {menuItems.map((item, index) => (
           <MenuItem
