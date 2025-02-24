9ad78efa21bbb28e2633fafbeba98e19a17bb9e7
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-02-24 21:47:18 +0200

    Update loading icon styles

--- frontend/src/components/Loading.jsx
+++ frontend/src/components/Loading.jsx
@@ -8,9 +8,9 @@ const Loading = ({ maxHeight = null, loadingText = "" }) => {
       style={{ maxHeight: maxHeight }}
     >
       <div className="relative mb-12 flex justify-center items-center">
-        <FaRegCircle className="w-8 h-8 absolute fill-current text-slate-300" />
+        <FaRegCircle className="w-8 h-8 md:w-10 md:h-10 xl:w-12 xl:h-12 absolute fill-current text-slate-300 dark:text-slate-600" />
         <div className="absolute flex justify-center items-center animate-rotate-infinite">
-          <AiOutlineLoading className="w-8 h-8 fill-current text-slate-500" />
+          <AiOutlineLoading className="w-8 h-8 md:w-10 md:h-10 xl:w-12 xl:h-12 fill-current text-slate-500 dark:text-slate-400" />
         </div>
       </div>
       <div className="text-slate-500">{loadingText}</div>
