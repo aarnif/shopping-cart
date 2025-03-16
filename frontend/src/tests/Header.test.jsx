2ab39e9948c9cbda48a3f06bb8a2d58eae78c582
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-03-16 15:55:57 +0200

    Add tests clicking nav items work

--- frontend/src/tests/Header.test.jsx
+++ frontend/src/tests/Header.test.jsx
@@ -1,7 +1,15 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, within } from "@testing-library/react";
 import { describe, test, vi, expect } from "vitest";
-import { MemoryRouter } from "react-router";
+import { MemoryRouter, useNavigate } from "react-router";
 import Header from "../components/Header.jsx";
+
+vi.mock("react-router", async () => {
+  const actual = await vi.importActual("react-router");
+  return {
+    ...actual,
+    useNavigate: vi.fn(),
+  };
+});
 
 const handleShowMenu = vi.fn();
 
@@ -30,5 +38,52 @@ describe("<Header />", () => {
   test("displays mobile nav", () => {
     renderHeaderComponent();
     expect(screen.getByTestId("mobile-nav")).toBeInTheDocument();
+  });
+
+  test("displays desktop page header", () => {
+    const navigate = vi.fn();
+    useNavigate.mockReturnValue(navigate);
+
+    renderHeaderComponent();
+    const desktopNav = screen.getByTestId("desktop-nav");
+    const pageHeaderButton = within(desktopNav).getByTestId("page-header");
+    expect(pageHeaderButton).toBeInTheDocument();
+    pageHeaderButton.click();
+    expect(navigate).toHaveBeenCalledWith("/");
+  });
+
+  test("displays mobile page header", () => {
+    const navigate = vi.fn();
+    useNavigate.mockReturnValue(navigate);
+
+    renderHeaderComponent();
+    const mobileNav = screen.getByTestId("mobile-nav");
+    expect(within(mobileNav).getByTestId("page-header")).toBeInTheDocument();
+  });
+
+  test("navigation works in desktop nav", () => {
+    const navigate = vi.fn();
+    useNavigate.mockReturnValue(navigate);
+
+    renderHeaderComponent();
+    const desktopNav = screen.getByTestId("desktop-nav");
+
+    const navItems = [
+      { testId: "nav-item-home", expectedPath: "/" },
+      { testId: "nav-item-shop", expectedPath: "/art" },
+      { testId: "nav-item-cart", expectedPath: "/cart" },
+    ];
+
+    navItems.forEach(({ testId, expectedPath }) => {
+      navigate.mockReset();
+
+      const navItem = within(desktopNav).getByTestId(testId);
+      expect(navItem).toBeInTheDocument();
+
+      const navButton = within(navItem).getByTestId(`${testId}-button`);
+      navButton.click();
+
+      expect(navigate).toHaveBeenCalledWith(expectedPath);
+    });
   });
 });
