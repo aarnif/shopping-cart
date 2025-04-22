53ef03cc378011f98dbd23e56dad2632a6db16e3
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-04-22 22:34:19 +0300

    Add iconSize and fontSize props

--- frontend/src/components/Loading.jsx
+++ frontend/src/components/Loading.jsx
@@ -1,7 +1,12 @@
 import { FaRegCircle } from "react-icons/fa6";
 import { AiOutlineLoading } from "react-icons/ai";
 
-const Loading = ({ maxHeight = null, loadingText = "" }) => {
+const Loading = ({
+  iconSize = "w-8 h-8 md:w-10 md:h-10 xl:w-12 xl:h-12",
+  maxHeight = null,
+  loadingText = "",
+  fontSize = "text-sm md:text-base xl:text-lg",
+}) => {
   return (
     <div
       data-testid="loading"
@@ -9,12 +14,18 @@ const Loading = ({ maxHeight = null, loadingText = "" }) => {
       style={{ maxHeight: maxHeight }}
     >
       <div className="relative mb-8 flex justify-center items-center">
-        <FaRegCircle className="w-8 h-8 md:w-10 md:h-10 xl:w-12 xl:h-12 absolute fill-current text-slate-300 dark:text-slate-600" />
+        <FaRegCircle
+          className={`${iconSize} absolute fill-current text-slate-300 dark:text-slate-600`}
+        />
         <div className="absolute flex justify-center items-center animate-rotate-infinite">
-          <AiOutlineLoading className="w-8 h-8 md:w-10 md:h-10 xl:w-12 xl:h-12 fill-current text-slate-500 dark:text-slate-400" />
+          <AiOutlineLoading
+            className={`${iconSize} fill-current text-slate-500 dark:text-slate-400`}
+          />
         </div>
       </div>
-      <p className="text-slate-100 xl:text-slate-700 xl:dark:text-slate-100 text-sm md:text-base xl:text-lg">
+      <p
+        className={`text-slate-100 xl:text-slate-700 xl:dark:text-slate-100 ${fontSize}`}
+      >
         {loadingText}
       </p>
     </div>
